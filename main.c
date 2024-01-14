092136bda2942d308c936f5b163e7cef725f8cce
ghemmourlehna <ghemmourlehna749@gmail.com>
2024-01-15 00:03:51 +0100

    Feature/Search + Insert screens

--- main.c
+++ main.c
@@ -44,6 +44,19 @@ void intArrayTo2DCharArray(int *arr, int size, char arrayValues[100][20]) {
     }
 }
 
+// Function to search for a value in an array
+// It returns true if the value is found, and sets the index parameter
+// If the value is not found, it returns false
+bool searchInArray(int arr[], int size, int value, int *index) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            *index = i; // Set the index of the found value
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(void)
 {
     const int screenWidth = 1024;
@@ -53,13 +66,17 @@ int main(void)
 
     char inputText[20] = "\0";
     char inputText_insert[20] = "\0";
+    char inputText_search[20] = "\0";
 
     int letterCount = 0;
     int letterCount_insert=0;
+    int letterCount_search=0;
 
     int framesCounter = 0;
     int framesCounter2 =0;
     int framesCounter_insert=0;
+    int framesCounter_search=0;
+
 
     int arraySize = 0;
     char arrayValues[100][20] = {0}; // Assuming a maximum array size of 100
@@ -73,20 +90,27 @@ int main(void)
     int spaceBetween = 20;
     int inputWidth = 120;
     int inputWidth_insert = 80;
+    int inputWidth_search = 80;
+
     Rectangle inputField = { textX + 325, textY + spaceBetween, inputWidth, 20 };
     Rectangle inputField_insert = { textX + 380, textY + spaceBetween, inputWidth_insert, 20 };
+    Rectangle inputField_search = { textX + 390, textY + spaceBetween, inputWidth_search, 20 };
 
     bool inputFieldActive = false;
     bool inputFieldActive_insert = false;
+    bool inputFieldActive_search = false;
 
     int arrayBoxWidth = 50;
     int arrayBoxHeight = 30;
     int arrayBoxSpacing = 5;
     int maxElementsPerLine = (screenWidth - 50) / (arrayBoxWidth + arrayBoxSpacing); // Calculate maximum elements per line
     int intArray[50] = {0};
+    int intArray_search[50] = {0};
 
     // Button positions and dimensions
     Rectangle insertButton = { 250, 600, 120, 40 };
+    Rectangle insertButton_search = { 550, 600, 120, 40 };
+
     Rectangle deleteButton = { 400, 600, 120, 40 };
     Rectangle searchButton = { 550, 600, 120, 40 };
     Rectangle sortButton = { 700, 600, 120, 40 };
@@ -105,7 +129,13 @@ int main(void)
     bool searchButtonEnabled = false;
     bool sortButtonEnabled = false;
     bool display_flag = false;
+    bool display_flag_search = false;
     bool arrayInputActive = false;
+    bool search_output = false;
+    int index = 0;
+    int newValue_search;
+
+    char searchText[50]; // Buffer to hold the score text
 
     ScreenState currentScreen = SCREEN_MAIN;
     // Button for returning to the main menu from the delete screen
@@ -141,6 +171,12 @@ int main(void)
 
                 if (CheckCollisionPointRec(mousePoint, searchButton) && searchButtonEnabled) {
                     searchButtonColor = LIGHTGRAY;
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        currentScreen = SCREEN_SEARCH; // Change to delete confirmation screen
+                        memset(inputText, 0, 20); // Clear the inputText
+                        letterCount = 0;
+                        inputFieldActive = false;
+                    }
                 } else {
                     searchButtonColor = GRAY;
                 }
@@ -234,6 +270,13 @@ int main(void)
 
                 if (CheckCollisionPointRec(mousePoint, searchButton)) {
                     searchButtonColor = LIGHTGRAY;
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        currentScreen = SCREEN_SEARCH; // Change to delete confirmation screen
+                        memset(inputText_search, 0, 20); // Clear the inputText
+                        letterCount_search = 0;
+                        inputFieldActive_search = false;
+                        display_flag_search = false;
+                    }
                 } else {
                     searchButtonColor = GRAY;
                 }
@@ -297,7 +340,95 @@ int main(void)
                 }
                 break;
             case SCREEN_SEARCH:
+                if (CheckCollisionPointRec(mousePoint, menuButton)) {
 
+                    menuButtonColor = BLUE;
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        currentScreen = SCREEN_MAIN; // Change to main screen
+                        
+                        memset(inputText_search, 0, 20); // Clear the inputText
+                        letterCount_search = 0;
+                        inputFieldActive_search = false;
+
+                        memset(arrayValues, 0, sizeof(arrayValues));
+                        arraySize = 0;
+                        display_flag = false;
+                        memset(intArray_search, 0, sizeof(intArray_search));
+                        arrayInputActive = false;
+            
+
+                    }
+                    
+                }else{menuButtonColor = DARKBLUE;}
+
+                // Check if mouse is over buttons and change button colors
+
+                if (CheckCollisionPointRec(mousePoint, deleteButton)) {
+                    deleteButtonColor = GRAY;
+
+                } else {
+                    deleteButtonColor = DARKGRAY;
+                }
+
+                if (CheckCollisionPointRec(mousePoint, insertButton_search)) {
+                    insertButtonColor = LIGHTGRAY;
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        currentScreen = SCREEN_INSERT; // Change to delete confirmation screen
+                        memset(inputText_search, 0, 20); // Clear the inputText
+                        letterCount_search = 0;
+                        inputFieldActive_search = false;
+                    }
+                } else {
+                    insertButtonColor = GRAY;
+                }
+
+                if (CheckCollisionPointRec(mousePoint, sortButton) ) {
+                    sortButtonColor = DARKGRAY;
+                } else {
+                    sortButtonColor = LIGHTGRAY;
+                }
+
+
+                if (CheckCollisionPointRec(mousePoint, (Rectangle){inputField_search.x, inputField_search.y - 20, inputField_search.width, 24})) {
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        inputFieldActive_search = true;
+                    }
+                } else {
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        inputFieldActive_search = false;
+                    }
+                }
+
+                if (inputFieldActive_search) {
+                    int key = GetCharPressed();
+                    while (key > 0) {
+                        if ((key >= 32) && (key <= 125) && (letterCount_search < 19)) {
+                            if (isdigit(key)) { // Check if key is a digit
+                                inputText_search[letterCount_search] = (char)key;
+                                letterCount_search++;
+                            }
+                        }
+
+                        key = GetCharPressed();
+                    }
+
+                    if (IsKeyPressed(KEY_BACKSPACE)) {
+                        if (letterCount_search > 0) {
+                            letterCount_search--;
+                            inputText_search[letterCount_search] = '\0';
+                        }
+                    }
+
+                    framesCounter_search++;
+                }
+                // Handle value search
+                if (inputFieldActive_search && IsKeyPressed(KEY_ENTER)) {
+                    ConvertCharArrayToIntArray(arrayValues, intArray_search, arraySize);
+                    newValue_search = atoi(inputText_search); // Convert input to integer
+
+                    search_output = searchInArray(intArray_search,arraySize,newValue_search,&index);
+                    display_flag_search = true;
+                }
                 break;
             case SCREEN_DELETE:
 
@@ -417,6 +548,142 @@ int main(void)
                         }
                         framesCounter2++;
                     }
+                    break;
+
+                case SCREEN_INSERT:
+                    ClearBackground(RAYWHITE);
+
+                    DrawText("Insert Feature", screenWidth / 2 - MeasureText("Insert Feature", 40) / 2, 50, 40, primaryColor);
+                    DrawText("Enter the value you want to insert:", textX, textY, 20, primaryColor);
+
+                    // Draw bottom line of input field
+                    DrawLineEx((Vector2){inputField_insert.x, inputField_insert.y}, (Vector2){inputField_insert.x + inputField_insert.width, inputField_insert.y}, 2, primaryColor);
+
+                    // Draw the input text
+                    if (letterCount_insert < 20 ) {
+                        DrawText(inputText_insert, textX + 380, textY  - 5, 20, inputTextColor); // Adjusted for alignment
+                    }
+
+                    // Draw blinking cursor
+                    if (inputFieldActive_insert && ((framesCounter_insert / 20) % 2 == 0) ) {
+                        DrawRectangle(textX + 380 + MeasureText(inputText_insert, 20), textY  - 5, 2, 20, inputTextColor);
+                    }
+
+
+
+                    
+                    // Handle new value insertion
+                    if (display_flag) {
+                        // Convert to 2D char array
+                        intArrayTo2DCharArray(intArray, arraySize, arrayValues);
+
+                        DrawText("Your new array is :", textX, textY + 100, 20, DARKBLUE);
+                        int line = 0;
+                        for (int i = 0; i < arraySize; i++) {
+                            // Calculate position with line breaks
+                            int x = textX + (i % maxElementsPerLine) * (arrayBoxWidth + arrayBoxSpacing);
+                            int y = textY + 140 + line * (arrayBoxHeight + arrayBoxSpacing);
+
+                            Rectangle arrayBox = {x, y, arrayBoxWidth, arrayBoxHeight};
+                            DrawRectangleRec(arrayBox, LIGHTGRAY);
+
+                            DrawText(arrayValues[i], arrayBox.x + 5, arrayBox.y + 5, 20, DARKGRAY);
+
+                            if (CheckCollisionPointRec(GetMousePosition(), arrayBox) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                                activeArrayInput = i;
+                            }
+
+                            if ((i + 1) % maxElementsPerLine == 0) {
+                                line++; // Move to next line after reaching max elements
+                            }
+                        }
+
+                    }
+
+                    // Draw 'Menu' button
+                    DrawRectangleRec(menuButton, menuButtonColor);
+                    DrawText("Menu", menuButton.x + 30, menuButton.y + 10, 20, WHITE);
+
+                    DrawRectangleRec(deleteButton, deleteButtonColor);
+                    DrawText("Delete", deleteButton.x + 10, deleteButton.y + 10, 20, WHITE);
+
+                    DrawRectangleRec(searchButton, searchButtonColor);
+                    DrawText("Search", searchButton.x + 10, searchButton.y + 10, 20, WHITE);
+
+                    DrawRectangleRec(sortButton, sortButtonColor);
+                    DrawText("Sort", sortButton.x + 10, sortButton.y + 10, 20, WHITE);
+                    
+                    break;
+                
+                case SCREEN_SEARCH:
+                    ClearBackground(RAYWHITE);
+
+                    DrawText("Search Feature", screenWidth / 2 - MeasureText("Search Feature", 40) / 2, 50, 40, primaryColor);
+                    DrawText("Enter the value you want to search:", textX, textY, 20, primaryColor);
+                    // Draw bottom line of input field
+                    DrawLineEx((Vector2){inputField_search.x, inputField_search.y}, (Vector2){inputField_search.x + inputField_search.width, inputField_search.y}, 2, primaryColor);
+
+                    // Draw the input text
+                    if (letterCount_search < 20 ) {
+                        DrawText(inputText_search, textX + 390, textY  - 5, 20, inputTextColor); // Adjusted for alignment
+                    }
+
+                    // Draw blinking cursor
+                    if (inputFieldActive_search && ((framesCounter_search / 20) % 2 == 0) ) {
+                        DrawRectangle(textX + 390 + MeasureText(inputText_search, 20), textY  - 5, 2, 20, inputTextColor);
+                    }
+
+                    // Draw 'Menu' button
+                    DrawRectangleRec(menuButton, menuButtonColor);
+                    DrawText("Menu", menuButton.x + 30, menuButton.y + 10, 20, WHITE);
+
+                    DrawRectangleRec(deleteButton, deleteButtonColor);
+                    DrawText("Delete", deleteButton.x + 10, deleteButton.y + 10, 20, WHITE);
+
+                    DrawRectangleRec(insertButton_search, insertButtonColor);
+                    DrawText("Insert", insertButton_search.x + 10, insertButton_search.y + 10, 20, WHITE);
+
+                    DrawRectangleRec(sortButton, sortButtonColor);
+                    DrawText("Sort", sortButton.x + 10, sortButton.y + 10, 20, WHITE);
+
+                    // Handle new value insertion
+                    if (display_flag_search) {
+
+                        DrawText("Your array is :", textX, textY + 100, 20, DARKBLUE);
+                        int line = 0;
+                        for (int i = 0; i < arraySize; i++) {
+                            // Calculate position with line breaks
+                            int x = textX + (i % maxElementsPerLine) * (arrayBoxWidth + arrayBoxSpacing);
+                            int y = textY + 140 + line * (arrayBoxHeight + arrayBoxSpacing);
+
+                            Rectangle arrayBox = {x, y, arrayBoxWidth, arrayBoxHeight};
+                            DrawRectangleRec(arrayBox, LIGHTGRAY);
+
+                            DrawText(arrayValues[i], arrayBox.x + 5, arrayBox.y + 5, 20, DARKGRAY);
+
+                            if (CheckCollisionPointRec(GetMousePosition(), arrayBox) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                                activeArrayInput = i;
+                            }
+
+                            if ((i + 1) % maxElementsPerLine == 0) {
+                                line++; // Move to next line after reaching max elements
+                            }
+                        }
+                        if (search_output){
+                        sprintf(searchText, "The value : %d was found at index : %d.", newValue_search,index);
+                        DrawText(searchText, textX, textY +200, 20, GREEN);}
+                        else{
+                            sprintf(searchText, "The value : %d was not found in you array.", newValue_search);
+                            DrawText(searchText, textX, textY + 200, 20, RED);}   
+                    }
+
+
+                    break;
+                case SCREEN_DELETE:
+
+                    break;
+                case SCREEN_SORT:
+
                     break;
 
 
